30be3e70e172c7635dc6a75ff99b0bb5b4ec6d88
Kosaretskiy_Maksim <ma.ko1995@mail.ru>
2026-06-16 16:53:48 +0300

    'fix: settings.py: update ALLOWED_HOSTS and add DJOSER settings'

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -105,15 +105,21 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
-    ],
-
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    ],
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'username',
+    'USER_CREATE_PASSWORD_RETYPE': False,
+    'SET_PASSWORD_RETYPE': False,
+    'SEND_ACTIVATION_EMAIL': False,
 }
